e7808e4f6c501812dc822603060e48a1fd053e40
Ravindra <ravindra.reddy1812@gmail.com>
2025-10-18 23:48:47 +0530

    DOM Modules using BusData example

--- DOM/BusExample/app.js
+++ DOM/BusExample/app.js
@@ -26,6 +26,55 @@ searchBtn.addEventListener("click",
       console.log(`Please enter your From and To address`);
       return;
     }
+
+    // Filter busesroutes
+    const filterBuses = buses.filter(
+      (bus) =>
+        bus.route.from.toLowerCase() === from && bus.route.to.toLowerCase() === to
+    );
+    console.log(filterBuses);
+
+    // check the length of filtered buses
+
+    if (filterBuses.length === 0) {
+      busResults.innerHTML = "<p class='text-xl font-bold text-pink-700'>No buses found</p>"
+    }
+
+    // Loop through buses and create a dynamic cards
+    filterBuses.forEach(bus => {
+      // create a parent container dynamically
+      const busCard = document.createElement("div");
+      busCard.className = "";
+
+      busCard.innerHTML =
+        `
+      <div>
+        <div>
+            <h2>${bus.operatorName}</h2>
+            <p>${bus.busType}</P>
+            <p>${new Date(bus.timings.departure).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+            -- > 
+            ${new Date(bus.timings.departure).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</P>
+        </div>
+        <div>
+            <p>${bus.fare.baseFare}</p>
+            <p>${bus.totalSeats}</p>   
+            <p>${bus.availableSeats}</p>                     
+        </div>
+        <div>
+          <p>${bus.amenities.map(
+            a => `<span>${a}</span>`
+          )}</p>
+        </div>
+        <div>
+          <div>${bus.rating}</div>
+          <button> View Seats </button>
+        </div>
+      </div>
+      `
+      busResults.appendChild(busCard);
+
+    })
   }
 )
 
